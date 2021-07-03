e7780f59d3780e1960af83912c6a70b81d91172b
furkan-cloud <firstinfirstout2020@gmail.com>
2021-07-03 15:56:24 +0300

    videos and video details api added

---
+++ pages/api/videos/[id].js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { videos } from "../../../videos";
+export default (req, res) => {
+  const { id } = req.query;
+
+  const video = videos.find((video) => video.id.toString() === id);
+  if (video) {
+    res.status(200).json(video);
+  } else {
+    res.status(404).json({
+      message: "Error. No such video!",
+    });
+  }
+};
